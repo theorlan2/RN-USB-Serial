e6e817f75f52f1e0ba6b709089e4666aaf767cd9
Dany Santos <theorlan2@hotmail.es>
2022-08-20 12:07:42 -0400

    add macros store and views...

--- app/scenes/macroCmdsList.tsx
+++ app/scenes/macroCmdsList.tsx
@@ -1,52 +1,51 @@
 import React, { FunctionComponent, useEffect, useState } from 'react'
-import { Alert, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native' 
+import { Alert, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import { StackNavigationProp } from '@react-navigation/stack';
+import { useTranslation } from 'react-i18next';
+import { RouteProp } from '@react-navigation/native';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 //
 import CardGroup from '../components/GroupsCmd/CardGroup';
 import ModalInfoFC from '../components/ModalInfoFC';
-import { GroupCmdModelView } from '../infrastructure/modelViews/GroupCmd';
 import { RootStackParamList } from '../routes/routesNames';
-import { getStoreData, setStoreData } from '../infrastructure/utils/utilsStore';
-import { MacroCmdModelView } from '../infrastructure/modelViews/MacroCmd'; 
-import { useTranslation } from 'react-i18next';
+import { MacroCmdModelView } from '../infrastructure/modelViews/MacroCmd';
 import { useTheme } from '../infrastructure/contexts/themeContexts';
+import { addMacro, deleteMacro, editMacro } from '../store/features/macroSlice';
+import { RootState } from '../store';
 //
 type MacroCmdsListScreenNavigationProp = StackNavigationProp<
     RootStackParamList,
     'MacroCmdsList'
 >;
 
-interface Props {
-    navigation: MacroCmdsListScreenNavigationProp;
+interface StateProps {
+    macros: MacroCmdModelView[] | undefined;
 }
+
+interface DispatchProps {
+    addMacro: (data: MacroCmdModelView) => void;
+    editMacro: (data: MacroCmdModelView) => void;
+    deleteMacro: (id: number) => void;
+}
+
+interface OwnProps {
+    navigation: MacroCmdsListScreenNavigationProp;
+    route: RouteProp<RootStackParamList, 'MacroCmds'>,
+}
+
+type Props = StateProps & DispatchProps & OwnProps;
 
 const MacroCmdsListScreen: FunctionComponent<Props> = (props) => {
 
     const { t } = useTranslation(['defaultData', 'macros'])
     const { colors } = useTheme();
-    const [macrosCmdsData, setGroupsCmdsData] = useState([] as GroupCmdModelView[]);
     const [showModalLoading, setShowModalLoading] = useState(true);
-
-    useEffect(() => {
-        getDataFromStorage();
-        return () => { };
-    }, [])
-
-    function getDataFromStorage() {
-        setShowModalLoading(true);
-        getStoreData('macrosCmds').then(r => {
-            setShowModalLoading(false);
-            if (r) {
-                setGroupsCmdsData(r);
-            }
-        }).catch(() => {
-            setShowModalLoading(false);
-        });
-    }
 
     function openMacro(id: number) {
         props.navigation.navigate('MacroCmds', { id: id });
     }
+
     function alertDelete(id: number) {
         Alert.alert(t('macros:loadMacros.delete.title'), t('macros:loadMacros.delete.description'),
             [
@@ -59,8 +58,7 @@ const MacroCmdsListScreen: FunctionComponent<Props> = (props) => {
     }
 
     function deleteMacro(id: number) {
-        let result = macrosCmdsData.filter((item: MacroCmdModelView) => item.id != id)
-        setStoreData('macrosCmds', result);
+        props.deleteMacro(id);
     }
 
     const styles = StyleSheet.create({
@@ -82,14 +80,30 @@ const MacroCmdsListScreen: FunctionComponent<Props> = (props) => {
         <View style={styles.mainCont} >
             <StatusBar backgroundColor={colors.headerAccent} barStyle="light-content" ></StatusBar>
             <ScrollView style={styles.scrollViewCont}   >
-                {macrosCmdsData.length < 1 && <View style={styles.contEmpty} >
+                {props.macros && props.macros.length < 1 && <View style={styles.contEmpty} >
                     <Text style={styles.textEmpty} >{t('macros:loadMacros.titles.empty')}</Text>
                 </View>}
-                {macrosCmdsData.map((item, key) => <CardGroup key={item.id + key} item={item} openGroup={openMacro} deleteGroup={alertDelete} />)}
+                {props.macros && props.macros.map((item, key) => <CardGroup key={item.id + key} item={item} openGroup={openMacro} deleteGroup={alertDelete} />)}
             </ScrollView>
             <ModalInfoFC closeModal={() => setShowModalLoading(false)} modalVisible={showModalLoading} title={t('macros:loadMacros.loading.title')} description={t('macros:loadMacros.loading.description')} loading={true} />
         </View>
     );
 }
 
-export default MacroCmdsListScreen;
+
+const mapStateToProps = (state: RootState) => {
+
+    return {
+        macros: state.macro.macros
+    }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        addMacro: (data: MacroCmdModelView) => dispatch(addMacro(data)),
+        editMacro: (data: MacroCmdModelView) => dispatch(editMacro(data)),
+        deleteMacro: (id: number) => dispatch(deleteMacro(id)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MacroCmdsListScreen);
